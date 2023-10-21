2577edae2586f955761c9b0465856a04114bf275
SteveSimms <simmsstev@gmail.com>
2023-10-21 09:55:19 -0700

    Changed set api example to SFC

--- src/app/examples/set-api/set-api.component.ts
+++ src/app/examples/set-api/set-api.component.ts
@@ -2,7 +2,58 @@ import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-set-api',
-  templateUrl: './set-api.component.html',
+  // templateUrl: './set-api.component.html',
+  template: `<div class="container mt-4 mb-4">
+  <div class="card">
+    <div class="card-header">
+      Using set method to update the number type value
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{ num() }}</h5>
+      <p class="card-text"></p>
+      <div class="btn-row">
+        <a (click)="addOne()" class="btn btn-primary">Add One</a>
+        <a (click)="reset()" class="btn btn-primary">Reset</a>
+        <a (click)="minusOne()" class="btn btn-primary">Minus One</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="card mt-5">
+    <div class="card-header">
+      Using set method to update the string type value
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{ name() }}</h5>
+      <p class="card-text"></p>
+      <div class="btn-row">
+        <a (click)="updateName()" class="btn btn-primary">Update name</a>
+        <a (click)="resetName()" class="btn btn-primary">Reset name</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="card mt-5">
+    <div class="card-header">
+      Using set method to update the boolean type value
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ isAdminUser() ? "Admin User" : "General User" }}
+      </h5>
+      <p class="card-text"></p>
+      <div class="btn-row">
+        <a (click)="updateToAdminUser()" class="btn btn-primary"
+          >Update to Admin User</a
+        >
+        <a (click)="updateToUser()" class="btn btn-primary"
+          >Update to General User</a
+        >
+      </div>
+    </div>
+  </div>
+</div>
+`,
   styleUrls: ['./set-api.component.css'],
 })
 export class SetApiComponent {
